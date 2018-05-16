fa28f6c41f7049e9ccbd4070e2db6c453057fc98
Vineet Choudhary <vineetchoudhary@live.in>
2018-05-16 16:00:32 +0530

    Initial Commit

---
+++ XFormsDemo/InitialPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XFormsDemo
+{
+    public partial class InitialPage : ContentPage
+    {
+        public InitialPage()
+        {
+            InitializeComponent();
+			//Padding = GetPaddingThickness();
+        }
+        
+
+		Thickness GetPaddingThickness()
+		{
+			switch (Device.RuntimePlatform)
+			{
+				case Device.iOS:
+					return new Thickness(0, 20, 0, 0);
+				case Device.Android:
+					return new Thickness(0, 10, 0, 0);
+				default:
+					return new Thickness(0, 5, 0, 0);
+			}
+		}
+    }
+}
---
+++ XFormsDemo/QuotesPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XFormsDemo
+{
+    public partial class QuotesPage : ContentPage
+    {
+		int currentQuoteIndex = 0;
+		String[] quotes = new String[] { "Hello frome the other side.", 
+			"I must've called a thousand times.", 
+			"To go over everything", 
+			"They say that time's supposed to heal ya", 
+			"But I ain't done much healing", 
+			"Hello frome the other side \nI must've called a thousand times \nTo go over everything \nThey say that time's supposed to heal ya \nBut I ain't done much healing"};
+        public QuotesPage()
+        {
+            InitializeComponent();
+			showQuote();
+        }
+
+		void NextQuoteButton_Handle_Clicked(object sender, System.EventArgs e)
+		{
+			showQuote();
+		}
+
+		void showQuote()
+		{
+			QuoteLabel.Text = quotes[currentQuoteIndex];
+            currentQuoteIndex = (currentQuoteIndex == (quotes.Length - 1)) ? 0 : (currentQuoteIndex + 1);
+		}
+    }
+}
