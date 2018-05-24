99709b9eb57c0814c46aebaf33e37eb0e9e8469a
Vineet Choudhary <vineetchoudhary@live.in>
2018-05-24 13:00:34 +0530

    Carousel

--- XFormsDemo/App.xaml.cs
+++ XFormsDemo/App.xaml.cs
@@ -4,6 +4,7 @@ using Xamarin.Forms.Xaml;
 using XFormsDemo.Navigations;
 using XFormsDemo.MasterDetails;
 using XFormsDemo.TabBarView;
+using XFormsDemo.Carousel;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace XFormsDemo
@@ -14,7 +15,7 @@ namespace XFormsDemo
         {
             InitializeComponent();
 
-			MainPage = new TabBarPage();
+			MainPage = new MainCarouselPage();
         }
 
         protected override void OnStart()
---
+++ XFormsDemo/Carousel/MainCarouselPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XFormsDemo.Carousel
+{
+	public partial class MainCarouselPage : CarouselPage
+    {
+        public MainCarouselPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
